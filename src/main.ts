a97ab0cabc1c02a44d010582f3e48d160ea527de
rishabhpoddar <rishabh.poddar@gmail.com>
2022-12-08 16:33:11 +0530

    adds code for loading opponent temtem

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log('hello 👋')
+import fs from "fs";
+
+const opponentTemtem: {
+    type: string[],
+    name: string
+}[] = []
+
+for (let i = 2; i < 4; i++) {
+    if (process.argv[i] !== undefined) {
+        let fileContent = JSON.parse(fs.readFileSync(process.argv[i], { encoding: 'utf8', flag: 'r' }));
+        let slashSplitted = process.argv[i].split("/");
+        fileContent.name = slashSplitted[slashSplitted.length - 1].split(".json")[0];
+        opponentTemtem.push(fileContent);
+    }
+}
+
+console.log("Opponent 1: " + opponentTemtem[0].name)
+if (opponentTemtem.length > 1) {
+    console.log("Opponent 2: " + opponentTemtem[1].name)
+}
